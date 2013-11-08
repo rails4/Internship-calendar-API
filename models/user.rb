ede2865584ed7471572a3eccf5f2f644c7104f7b
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2013-11-08 14:38:46 +0100

    [#45] Add field password with validations and unit tests.
    Add field password.
    Add validation of type, presence and uniqueness of email.
    Add validation of type, presence and confirmation of password.

--- models/user.rb
+++ models/user.rb
@@ -2,6 +2,8 @@ class User
   include Mongoid::Document
 
   field :email, type: String
+  field :password, type: String
 
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -4,4 +4,8 @@ describe User do
   it { should have_field(:email).of_type(String) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
+
+  it { should have_field(:password).of_type(String) } 
+  it { should validate_presence_of(:password) }
+  it { should validate_confirmation_of(:password) }
 end
