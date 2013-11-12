d76a8947e86c728a862614e3ab5b74ace7185f30
Sebastian Hebel <sebah1989@gmail.com>
2013-11-12 11:36:25 +0100

    [49] changed password in User model to be hashed

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'mongoid'
+gem 'bcrypt-ruby', '~> 3.0.0'
 
 group :test do
   gem 'rspec'
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 
 describe User do
   it { should have_field(:email).of_type(String) }
-  it { should have_field(:password).of_type(String) }
-
+  it { should have_field(:password_digest).of_type(String) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_format_of(:email).to_allow("calendar@calendar.com").not_to_allow("foo bar") }
