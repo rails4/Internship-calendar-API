176dd1e41bbe96cb57e2b86238328261dd1c0b38
Oskar Szrajer <oskarszrajer@gmail.com>
2013-11-14 13:37:25 +0100

    setup proper sinatra test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,22 @@ require_relative '../calendar'
 require 'mongoid-rspec'
 require 'rack/test'
 
+Sinatra::Base.set :environment, :test
+Sinatra::Base.set :run, false
+Sinatra::Base.set :raise_errors, true
+Sinatra::Base.set :logging, false
+
+Mongoid.load!("config/mongoid.yml", 'test')
+
 module CalendarApp
   include Rack::Test::Methods
 
   def app
     Calendar
+  end
+
+  def parsed_last_response
+    @parsed_last_response ||= JSON.parse(last_response.body)
   end
 end
 
