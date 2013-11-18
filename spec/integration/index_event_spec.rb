652db66ffc202c205b527acacf0bc53a550b4f9c
Sebastian Hebel <sebah1989@gmail.com>
2013-11-18 14:59:09 +0100

    corrections in tests

--- spec/integration/index_event_spec.rb
+++ spec/integration/index_event_spec.rb
@@ -2,26 +2,23 @@ require 'spec_helper'
 
 describe 'Index event' do
   include CalendarApp
+  subject! { get '/events' }
   
   it 'should return HTTP 200 code' do
-    get '/events'
     last_response.status == 200
   end
 
   it 'should return response in JSON' do
-    get '/events'
     last_response.header['Content-type'] == "application/json;charset=utf-8"
   end
 
   it 'should return a hash' do
-    get '/events'
     parsed_last_response.should be_a(Hash)
   end
   
   context 'when no event records exist' do
-    it 'should return an empty hash' do
-      get '/events'
-      parsed_last_response["message"] == {}
+    it 'should return an empty array' do
+      parsed_last_response["message"] == []
     end
   end
 
@@ -32,7 +29,7 @@ describe 'Index event' do
     end
 
     it 'should contain proper number of events' do
-      parsed_last_response["message"].size.should == eq(5)
+      parsed_last_response["message"].size.should == 5
     end
   end
 end
