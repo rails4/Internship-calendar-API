a8094676c009acad85b789fef01e76fd0444ec66
Robert Tomczak <ufo22@onet.eu>
2013-11-20 15:02:40 +0100

    [#61] Add changes

--- calendar.rb
+++ calendar.rb
@@ -10,12 +10,13 @@ require_relative 'models/event'
 Mongoid.load!("config/mongoid.yml")
 
 class PasswordInvalid < StandardError; end
+class AccessDenied < StandardError; end
 
 class Calendar < Sinatra::Base
 
   before {
     content_type :json
-    unless ['/status'].include?(request.path_info)
+    unless ['/status', '/login', '/events', '/user', '/event'].include?(request.path_info)
       require_param(params[:token])
       begin
         @current_user = User.find_by(token: params[:token])
@@ -30,16 +31,12 @@ class Calendar < Sinatra::Base
   end
 
   # User
-  get '/users' do
-    json_message(User.all)
-  end
-
   get '/users/:id' do
     begin
-      raise AccessDenied if params[:id] != @current_user.id
+      raise AccessDenied if params[:id] != @current_user.id.to_s
       json_message(@current_user)
-    rescue Mongoid::Errors::DocumentNotFound
-      json_error(404, 'User not found')
+    rescue AccessDenied
+      json_error(403, 'Forbidden')
     end
   end
 
@@ -53,7 +50,7 @@ class Calendar < Sinatra::Base
       if user = user.authenticate(params[:password])
         json_message({token: user.token})
       else
-        raise PasswordInvalid user.authenticate(params[:password])
+        raise PasswordInvalid
       end
     rescue PasswordInvalid
       json_error(403, 'Forbidden')
@@ -76,10 +73,8 @@ class Calendar < Sinatra::Base
 
   put '/users/:id' do
     begin
-      User.find(params[:id]).update_attributes!(email: params[:email], password: params[:password])
+      @current_user.find(params[:id]).update_attributes!(email: params[:email], password: params[:password])
       json_message('User updated successfully')
-    rescue Mongoid::Errors::DocumentNotFound
-      json_error(404, 'User not found')
     rescue Mongoid::Errors::Validations => e
       if e.to_s.include?('Email is already taken')
         json_error(409, 'Email is already taken')
@@ -119,16 +114,16 @@ class Calendar < Sinatra::Base
 
   get '/event/:id' do
     begin
-      event = Event.find(params[:id])
-      puts "#{event.private}"
-      if (event.private == false) || (event.private == true && event.token == @current_user.token)
-        json_message(event)
-      else
-        json_error(401, "Don't have rights to show this event")
-      end
+    event = Event.find(params[:id])
+    if (event.private == false) || (event.private == true && event.users.map(&:token).any?(@current_user.token))#(event.private == true && event.token == @current_user.token)
+      json_message(event)
+    else
+      json_error(401, "Don't have rights to show this event")
+    end
     rescue Mongoid::Errors::DocumentNotFound
       json_error(404, "Expected event with given id is not found!")
     end
+
   end
 
   delete '/event/:id' do
