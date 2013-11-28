c6312a89a768a46bb5bb0cbee5c86809c3c99b02
Oskar Plichta <oskar.plichta22@gmail.com>
2013-11-28 12:55:20 +0100

    [#80] Fixed tests for delete_user_from_event.rb

--- spec/integration/delete_user_from_event.rb
+++ spec/integration/delete_user_from_event.rb
@@ -15,14 +15,17 @@ describe 'Delete user from event' do
       end
 
       it 'response should be in JSON' do
+        subject
         last_response.header['Content-Type'].should == 'application/json;charset=utf-8'
       end
 
       it "should return 200 HTTP code" do
+        subject
         last_response.status.should == 200
       end
 
       it "should return JSON formatted message {'message': 'User has been removed from event'}" do
+        subject
         parsed_last_response["message"].should == "User has been removed from event"
       end
 
@@ -33,11 +36,7 @@ describe 'Delete user from event' do
       end
 
       context 'for valid params' do
-        let(:user) { create(:user) }
         let(:user2) { create(:user, email: "now@example.com") }
-        let(:event) { create(:event, owner: user.id) }
-
-       
       
         it 'should return 200 HTTP code' do
           event.users << user2
@@ -45,11 +44,60 @@ describe 'Delete user from event' do
           last_response.status.should == 200
         end
 
-        it 'should add user to event' do
+        it 'should add 2 users to event and remove one' do
           expect {
-            event.users << user2
+            event.users << user2  
             subject
           }.to change { event.reload.users.count }.by(1)
+        end
+
+        it 'id unremoved user should match with user2.id ' do 
+            event.users << user2         
+            event.reload.users.last.id.should == user2.id
+        end
+      end
+
+      context 'for invalid params' do
+        context 'User not exist in base nor in event' do
+          subject! do
+            delete_user_from_event(base_params.merge(token: user.token, event_id: event.id, email: nil))
+          end
+
+          it 'response should be in JSON' do
+            subject
+            last_response.header['Content-Type'].should == 'application/json;charset=utf-8'
+          end
+
+          it "should return 404 HTTP code" do
+            subject
+            last_response.status.should == 404
+          end
+
+          it "should return JSON formatted message {'message': 'User in event not found!'}" do
+            parsed_last_response["message"].should == "User in event not found!"
+          end
+        end
+
+        context 'User exist in base but not in event' do
+          let(:user2) { create(:user, email: "now@example.com") }
+          subject! do
+            delete_user_from_event(base_params.merge(token: user.token, event_id: event.id, email: user2.email))
+          end
+
+          it 'response should be in JSON' do
+            subject
+            last_response.header['Content-Type'].should == 'application/json;charset=utf-8'
+          end
+
+          it "should return 404 HTTP code" do
+            subject
+            last_response.status.should == 404
+          end
+
+          it "should return JSON formatted message {'message': 'User in event not found!'}" do
+            subject
+            parsed_last_response["message"].should == "User in event not found!"
+          end
         end
       end
     end
